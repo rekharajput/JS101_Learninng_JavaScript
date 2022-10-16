7da79cb963cbdcb7ac2276ecf6b2155fa819efbd
rekharajput <rakharajput737@gmail.com>
2022-10-16 21:38:59 +0530

    First commit

---
+++ Introduction to JS(Variables)/problem3.js
@@ -0,0 +1,16 @@
+let a=5;
+let b=8;
+if(a>b)
+{
+  console.log(a);
+  
+}
+else if(b>a)
+{
+  
+ console.log(b) ;
+  }
+else
+{
+  console.log("both equal");
+}
---
+++ Introduction to JS(Variables)/problem4.js
@@ -0,0 +1,15 @@
+let username = "rekha";
+let password = "123";
+let user = "abc";
+let pass = "123";
+if (user==username)  
+{
+  if (pass==password)
+  {
+    console.log("user can login")
+  }
+}
+else
+{
+ console.log("user can not login")  
+}
